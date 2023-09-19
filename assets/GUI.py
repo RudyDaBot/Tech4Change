bd1194ca8cdcc0157651ef23bd3ec61b43755a26
RudyDaBot <cheekurudy@gmail.com>
2023-09-19 20:44:26 +0530

    test

--- assets/GUI.py
+++ assets/GUI.py
@@ -1,25 +1,32 @@
 from tkinter import *
 
 window=Tk()
+window.title('Tech4Change')
+window.geometry("800x730")
 
 def bruh() :
     print("BRUH")
 
+coordsX = window.winfo_screenwidth()
+coordsY = window.winfo_screenheight()
 
 # this is a button
 btn=Button(window, text="click me daddy", fg='black', font=('Comic Sans MS', 10), command=bruh)
-btn.place(x=80, y=100)
+btn.place(x=coordsX/2, y=coordsY)
 
 # this is a label
 lbl=Label(window, text="oh yes daddy oh yeah", fg='black', font=("Comic Sans MS", 16))
-lbl.place(x=60, y=50)
+lbl.place(x=coordsX/2, y=coordsY/2)
 
 # this is a entry box
 txtfld=Entry(window, text="enter in to me daddy", bd=5, font=("Comic Sans MS", 10))
-txtfld.place(x=80, y=150)
+txtfld.place(x=180, y=250)
+
+window.mainloop()
+
+while (True) :
+    print(coordsX, coordsY)
 
 
 # this is some setup to render those things
-window.title('Tech4Change')
-window.geometry("300x200+10+10")
-window.mainloop()
+
